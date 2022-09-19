8bd00d950e3ed9c665123b52e15cd8d992b7c1bc
Tomas Fiers <tomas.fiers@gmail.com>
2022-09-19 19:01:55 +0100

    fix typo: all caps in `PyNULL`

--- src/init.jl
+++ src/init.jl
@@ -1,4 +1,4 @@
-# On `PyNull()` and `copy!`:
+# On `PyNULL()` and `copy!`:
 # https://github.com/JuliaPy/PyCall.jl/issues/699#issuecomment-504616552
 
 """
@@ -8,14 +8,14 @@ Matplotlib's style settings [1]. Note that directly editing `mpl.rcParams` has n
 - [1] https://matplotlib.org/stable/tutorials/introductory/customizing.html#the-default-matplotlibrc-file
 - [2] https://github.com/JuliaPy/PyPlot.jl#modifying-matplotlibrcparams
 """
-const rcParams = PyNull()
+const rcParams = PyNULL()
 
 """
 A copy of the initial `mpl.rcParams`. Note that we do not use `mpl.rcParamsDefault` or
 `mpl.rcParamsOrig`, as these are different to what's actually used by default (e.g. in a
 Jupyter notebook).
 """
-const rcParams_original = PyNull()
+const rcParams_original = PyNULL()
 
 function __init__()
     copy!(rcParams_original, mpl.rcParams)
