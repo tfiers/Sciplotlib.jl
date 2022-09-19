485c0024e5a5f7864849da774ef29308a54158f3
Tomas Fiers <tomas.fiers@gmail.com>
2022-09-19 17:48:11 +0100

    `__init__` with `copy!` and `PyNull()`

--- src/init.jl
+++ src/init.jl
@@ -1,3 +1,6 @@
+# On `PyNull()` and `copy!`:
+# https://github.com/JuliaPy/PyCall.jl/issues/699#issuecomment-504616552
+
 """
 Matplotlib's style settings [1]. Note that directly editing `mpl.rcParams` has no effect
 [2]. Editing this object however does work.
@@ -5,18 +8,18 @@ Matplotlib's style settings [1]. Note that directly editing `mpl.rcParams` has n
 - [1] https://matplotlib.org/stable/tutorials/introductory/customizing.html#the-default-matplotlibrc-file
 - [2] https://github.com/JuliaPy/PyPlot.jl#modifying-matplotlibrcparams
 """
-rcParams = nothing
+const rcParams = PyNull()
 
 """
 A copy of the initial `mpl.rcParams`. Note that we do not use `mpl.rcParamsDefault` or
 `mpl.rcParamsOrig`, as these are different to what's actually used by default (e.g. in a
 Jupyter notebook).
 """
-rcParams_original = nothing
+const rcParams_original = PyNull()
 
 function __init__()
-    global rcParams_original = copy(mpl.rcParams)
-    global rcParams = PyPlot.PyDict(mpl."rcParams")  # String quotes prevent conversion from
-                                                     # Python to Julia dict.
+    copy!(rcParams_original, mpl.rcParams)
+    copy!(rcParams, PyPlot.PyDict(mpl."rcParams")) # String quotes prevent conversion from
+    #                                              # Python to Julia dict.
     set_mpl_style!(sciplotlib_style)
 end
