492789a69ab81c1bda517d26f336fdc626c37943
Tomas Fiers <tomas.fiers@gmail.com>
2023-09-27 17:18:14 +0200

    `deemph_middle_ticks`: ticks now totally off (both major and minor).

--- src/deemph.jl
+++ src/deemph.jl
@@ -20,40 +20,46 @@ function deemph(part::Symbol, ax; color = lightgrey)
 end
 
 
-function deemph_middle_ticks(x, black = 0.24)
+function deemph_middle_ticks(x; kw...)
     if isinstance(x, plt.Figure)
         for ax in x.axes
-            deemph_middle_ticks(ax, black)
+            deemph_middle_ticks(ax; kw...)
         end
     elseif isinstance(x, plt.Axes)
-        deemph_middle_ticks(x.xaxis, black)
-        deemph_middle_ticks(x.yaxis, black)
+        deemph_middle_ticks(x.xaxis; kw...)
+        deemph_middle_ticks(x.yaxis; kw...)
     else
-        _deemph_middle_ticks(x, black)
+        _deemph_middle_ticks(x; kw...)
     end
     x
 end
 
 isinstance(x, T) = pyconvert(Bool, pybuiltins.isinstance(x, T))
 
-function _deemph_middle_ticks(axis, black)
-    color = as_mpl_type(Gray(1 - black))
+function _deemph_middle_ticks(
+    axis;
+    ticklabel_black = 0.24,
+    major_tick_black = 0,
+    minor_tick_black = 0,
+)
+    gray(black) = as_mpl_type(Gray(1 - black))
     ticklabels = pyconvert(Vector, axis.get_ticklabels())
     N = length(ticklabels)
     N_to_deemph = length(ticklabels) - 2
     N_to_deemph > 0 || return
     for t in ticklabels[2:end-1]
-        t.set_color(color)
+        t.set_color(color = gray(ticklabel_black))
     end
     ticks = get_ticks(axis)
     N = length(axis.get_ticklocs())
     major_ticks = ticks[1:N]
     minor_ticks = ticks[(N+1):end]
+    # Thx https://stackoverflow.com/a/33698352/2611913 for `tick._apply_params()`
     for t in major_ticks[2:end-1]
-        t._apply_params(; color)  # Thx https://stackoverflow.com/a/33698352/2611913
+        t._apply_params(color = gray(major_tick_black))
     end
     for t in minor_ticks
-        t._apply_params(; color)
+        t._apply_params(color = gray(minor_tick_black))
     end
 end
 
